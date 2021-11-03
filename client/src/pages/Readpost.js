e840db58d909c5afef73218400dca8fbf7873af9
minjikim <alswl1500@naver.com>
2021-11-04 00:46:16 +0900

    style: 탭 스페이스로 변환

--- client/src/pages/Readpost.js
+++ client/src/pages/Readpost.js
@@ -15,85 +15,86 @@ import dayjs from 'dayjs';
 import CommentList from '../containers/CommentList';
 
 const PostWrapper = styled.div`
-	min-height      : 600px;
+  min-height      : 600px;
 `
 
 const Line = styled.hr`
-	margin          : 16px 0px;
-	height          : 0.5px;
-	background-color: #46508c;
+  margin          : 16px 0px;
+  border          : 0;
+  height          : 1px;
+  background-color: #46508c;
 `
 
 const Date = styled.div`
-	display         : inline-block;
-	font-size       : 12px;
-	color           : #888888;
+  display         : inline-block;
+  font-size       : 12px;
+  color           : #888888;
 `
 
 const EditButton = styled.button`
-	border          : none;
-	background      : none;
-	display         : inline-block;
-	cursor          : pointer;
-	font-size       : 12px;
-	color           : #888888;
+  border          : none;
+  background      : none;
+  display         : inline-block;
+  cursor          : pointer;
+  font-size       : 12px;
+  color           : #888888;
 `
 
 const Wrapper = styled.div`
-	display         : flex;
-	justify-content : space-between;
-	align-items     : center;
+  display         : flex;
+  justify-content : space-between;
+  align-items     : center;
 `
 
 const ContentWrapper = styled.div`
-	min-height      : 250px;
+  min-height      : 250px;
 `
 
 const Title = styled.h1`
-	margin          : 0px;
-	color           : #333333;
-	margin-bottom   : 12px;
+  margin          : 0px;
+  color           : #333333;
+  margin-bottom   : 12px;
 `
 
 const Content = styled.p`
-	font-size       : 14px;
-	line-height     : 20px;
-	font-weight     : 500;
-	color           : #565656;
+  font-size       : 14px;
+  line-height     : 20px;
+  font-weight     : 500;
+  color           : #565656;
 `
 
 const ReadPost = () => {
 
-	const { id } = useParams();
-	const [openAlert, setOpenAlert] = React.useState(false);
-	const [post, setPost] = React.useState([]);
-	const callPostApi = async()=>{
+  const { id } = useParams();
+  const [openAlert, setOpenAlert] = React.useState(false);
+  const [post, setPost] = React.useState([]);
+  const callPostApi = async()=>{
     const response = await axios.get('http://localhost:4000/read/'+ id);
     return response.data;
   }
 
-	React.useEffect(()=>{
-		callPostApi()
+  React.useEffect(()=>{
+    callPostApi()
     .then(res=>{
       setPost(res.data[0]);
     })
     .catch(err=>console.log(err));
   }, []);
 
-	const handleOpenAlert = () => {
+  const handleOpenAlert = () => {
     setOpenAlert(true);
   };
 
-	const handleCloseAlert = () => {
+  const handleCloseAlert = () => {
     setOpenAlert(false);
   };
 
-	const handleDelete = (e) => {
+  const handleDelete = (e) => {
     e.preventDefault();
     axios.delete('http://localhost:4000/delete/post/'+id) 
     .then(function ( response ) { 
       console.log( response ); 
-			window.location.href="/";
+      window.location.href="/";
     }) 
     .catch(error => { 
       console.log( 'error : ',error.response ) 
@@ -101,51 +102,51 @@ const ReadPost = () => {
   }
 
   return (
-		<Container maxWidth="md">
-			<PostWrapper>
-				<Title>{post.title}</Title>
-				<Wrapper>
-					<Date>{dayjs(post.date).format("YYYY년 MM월 DD일 HH:mm")}</Date>
-					<div>
-						<EditButton
-							onClick={() => window.location.href="/write/"+id}
-						>
-							수정
-						</EditButton>
-						<EditButton onClick={handleOpenAlert}>
-							삭제
-						</EditButton>
-					</div>
-				</Wrapper>
-				<Line/>
-				<ContentWrapper>
-					<Content>
-						{post.content}
-					</Content>
-				</ContentWrapper>
-				<Line/>
-				<CommentList id={id} post={post}/>
-				<Dialog
-					open={openAlert}
-					onClose={handleCloseAlert}
-					aria-labelledby="alert-dialog-title"
-					aria-describedby="alert-dialog-description"
-				>
-					<DialogTitle id="alert-dialog-title">
-						{"포스트 삭제"}
-					</DialogTitle>
-					<DialogContent>
-						<DialogContentText id="alert-dialog-description">
-							정말 글을 삭제하시겠습니까?
-						</DialogContentText>
-					</DialogContent>
-					<DialogActions>
-						<Button onClick={handleCloseAlert}>아니오</Button>
-						<Button onClick={handleDelete} autoFocus>네</Button>
-					</DialogActions>
-				</Dialog>
-			</PostWrapper>
-		</Container>
+    <Container maxWidth="md">
+      <PostWrapper>
+        <Title>{post.title}</Title>
+        <Wrapper>
+          <Date>{dayjs(post.date).format("YYYY년 MM월 DD일 HH:mm")}</Date>
+          <div>
+            <EditButton
+              onClick={() => window.location.href="/write/"+id}
+            >
+              수정
+            </EditButton>
+            <EditButton onClick={handleOpenAlert}>
+              삭제
+            </EditButton>
+          </div>
+        </Wrapper>
+        <Line/>
+        <ContentWrapper>
+          <Content>
+          {post.content}
+          </Content>
+        </ContentWrapper>
+        <Line/>
+        <CommentList id={id} post={post}/>
+        <Dialog
+          open={openAlert}
+          onClose={handleCloseAlert}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          >
+          <DialogTitle id="alert-dialog-title">
+            {"포스트 삭제"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              정말 글을 삭제하시겠습니까?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseAlert}>아니오</Button>
+            <Button onClick={handleDelete} autoFocus>네</Button>
+          </DialogActions>
+        </Dialog>
+      </PostWrapper>
+    </Container>
   )
 }
 
